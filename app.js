dad44d5bac7c3e831fba116612c3ce354733bf42
sohail60 <sohailatwork10@gmail.com>
2022-09-13 19:07:47 +0530

    Updated

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 //jshint esversion:6
 
 // requires
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require('mongoose');
-const encrypt=require('mongoose-encryption');           // Used for encryption key protection
+// const encrypt=require('mongoose-encryption');           // Used for encryption key protection
+// const mb5=require('mb5');       // Used for mb5 protection
+// const bcrypt=require('bcrypt');     // Used for bcrypt protection
+// const saltRounds=10;     // Used for bcrypt protection
+const session=require('express-session');       // Used for Passport Encryption
+const passport=require('passport');             // Used for Passport Encryption         
+const passportLocalMongoose=require('passport-local-mongoose');     // Used for Passport Encryption
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
+app.use(session({           // Used for Passport Encryption
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialzed: false
+}));
+
+app.use(passport.initialize());         // Used for Passport Encryption
+app.use(passport.session());            // Used for Passport Encryption
+
 // Mongoose
 mongoose.connect('mongodb://localhost:27017/secretsDB',{useNewUrlParser: true});
+// mongoose.set("useCreateIndex", true);
 
 const userSchema= new mongoose.Schema({
     email: String,
     password: String
 })
 
-const secret=process.env.SECRET;     // Used as Environment Variable
+userSchema.plugin(passportLocalMongoose);       // Used for Passport Encryption
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});     // Used for encryption key protection
+// const secret=process.env.SECRET;     // Used as Environment Variable
+
+// userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});     // Used for encryption key protection
 
 const User = new mongoose.model('User',userSchema);
+
+// Used for Passport Encryption
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Routing
 app.get('/',function(req,res){
@@ -40,39 +63,89 @@ app.get('/login',function(req,res){
     res.render('login');
 });
 
-app.post('/register',function(req,res){
-    const newUser=new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+// app.post('/register',function(req,res){
+//     bcrypt.hash(req.body.password, saltRounds, function
+//         (err,hash){         // Used for bcrypt encryption
+            
+//     const newUser=new User({
+//         email: req.body.email,
+//         password: hash          // Used for bcrypt encryption
+//         // password: md5(req.body.password)        // Used for md5 protection
+//     })
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    })
+//     newUser.save(function(err){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             res.render('secrets');
+//         }
+//     })
+//     })
+// })
+
+// app.post('/login',function(req,res){
+//     const email= req.body.email;
+//     const password= req.body.password;
+//     // const password= md5(req.body.password);     // Used for md5 protection
+    
+//     User.findOne({email: email},function(err,foundUser){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             if(foundUser){
+//                 bcrypt.compare(password,foundUser.password, function(err,result){
+//                     if(result===true){
+//                         res.render('secrets');
+//                     }
+//                 })
+//             }
+//         }
+//     })
+// })
+
+// Used for Passport Encryption
+app.get('/secrets',function(req,res){
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.post('/register',function(req,res){
+        User.register({email:req.body.email}, req.body.password, function(err,user){
+            if(err){
+                console.log(err);
+                res.redirect('/register');
+            } else {
+                passport.authenticate('local')(req,res,function(){
+                    res.redirect('/secrets');
+                });
+            }
+        });
 })
 
 app.post('/login',function(req,res){
-    const email= req.body.email;
-    const password= req.body.password;
-    
-    User.findOne({email: email},function(err,foundUser){
+    const user=new User({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    req.login(user,function(err){
         if(err){
             console.log(err);
         } else {
-            if(foundUser){
-                bcrypt.compare(password,foundUser.password, function(err,result){
-                    if(result===true){
-                        res.render('secrets');
-                    }
-                })
-            }
+            passport.authenticate('local')(req,res,function(){
+                res.redirect('/secrets');
+            });
         }
     })
 })
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
 
 // Listening
 const port=3000 || process.env.PORT;
